062ca3bb6855dc47d4f4d66f7ac33142226f4264
Andres Betancourt <andresbetancourtrc@gmail.com>
2025-08-07 14:36:05 -0500

    redireccionar a rootpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
@@ -14,6 +14,7 @@ function App() {
       <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/front_portafolio" replace />} />
         <Route path="/front_portafolio" element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
