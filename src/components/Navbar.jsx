fe132f7e4ce1d11f5866e88cac25e66e03bc5660
Andres Betancourt <andresbetancourtrc@gmail.com>
2025-08-07 14:26:26 -0500

    correcion del link del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ export const Navbar = () => {
   return (
     <header className="bg-[#10433B] text-[#E47A5D] p-4">
       <nav className="flex flex-col md:flex-row justify-around"> 
-        <Link to="/" className="text-2xl font-bold mb-4 md:mb-0">
+        <Link to="/front_portafolio" className="text-2xl font-bold mb-4 md:mb-0">
           Fuindein 
         </Link>
         <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
             <li>
-                <Link to="/" className={location.pathname === '/front_portafolio/'?'text-[#ECBD47]':'text-[#E47A5D]'}>Inicio</Link>
+                <Link to="/front_portafolio" className={location.pathname === '/front_portafolio'?'text-[#ECBD47]':'text-[#E47A5D]'}>Inicio</Link>
             </li>
             <li>
                 <Link to="/about" className={location.pathname === '/about'? 'text-[#ECBD47]':'text-[#E47A5D]'}>Sobre mí</Link>
